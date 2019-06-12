d38e2bb6ba32afb87e62029c06606f4639e98199
Alex Solomaha <cyanofresh@gmail.com>
2019-06-13 02:03:43 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,53 @@ WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
+const char *stateToString(uint8_t state = 2) {
+    if (state == 2) {
+        state = digitalRead(config::RELAY_PIN);
+    }
+
+    return state == HIGH ? "false" : "true";
+}
+
 void connectToWifi() {
-    Serial.println("Connecting to Wi-Fi...");
+    Serial.println(F("Connecting to Wi-Fi..."));
     WiFi.begin(config::WIFI_SSID, config::WIFI_PASSWORD);
 }
 
 void connectToMqtt() {
-    Serial.println("Connecting to MQTT...");
+    Serial.println(F("Connecting to MQTT..."));
     mqttClient.connect();
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP &event) {
-    Serial.println("Connected to Wi-Fi.");
-
     connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
-    Serial.print("Disconnected from Wi-Fi: ");
+    Serial.print(F("Disconnected from Wi-Fi: "));
     Serial.println(event.reason);
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
 
     mqttReconnectTimer.detach();
     wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void onMqttConnect(bool) {
-    Serial.println("Connected to MQTT.");
-    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println(F("Connected to MQTT."));
+    digitalWrite(LED_BUILTIN, HIGH);
 
     // Subscribe to topics:
     mqttClient.subscribe("switch/room1-fan/set", 0);
     mqttClient.subscribe("switch/room1-fan/toggle", 0);
-    mqttClient.subscribe("device/room1-fan", 0);
 
     // Send current state
-    mqttClient.publish("switch/room1-fan", 0, false, digitalRead(config::RELAY_PIN) == HIGH ? "false" : "true");
+    mqttClient.publish("switch/room1-fan", 0, false, stateToString());
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-    Serial.print("Disconnected from MQTT. Reason: ");
+    Serial.print(F("Disconnected from MQTT. Reason: "));
     Serial.println((int) reason);
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
 
     if (WiFi.isConnected()) {
         mqttReconnectTimer.once(2, connectToMqtt);
@@ -89,14 +94,11 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties,
 }
 
 void setup() {
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    Serial.begin(115200);
-    Serial.println();
-    Serial.println();
-
+    pinMode(LED_BUILTIN, OUTPUT);
     pinMode(config::RELAY_PIN, OUTPUT);
     pinMode(config::BTN_PIN, INPUT_PULLUP);
+
+    digitalWrite(LED_BUILTIN, LOW);
 
     bool lastState = HIGH;  // off by default
 
@@ -104,6 +106,10 @@ void setup() {
     EEPROM.get(0, lastState);
 
     digitalWrite(config::RELAY_PIN, lastState);
+
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();
 
     wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
     wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
@@ -133,7 +139,7 @@ void loop() {
 
                 digitalWrite(config::RELAY_PIN, newState);
 
-                mqttClient.publish("switch/room1-fan", 0, false, newState == HIGH ? "false" : "true");
+                mqttClient.publish("switch/room1-fan", 0, false, stateToString(newState));
 
                 Serial.println("Button pressed");
 
